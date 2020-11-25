97df9c1df18ee51fe96806ada93f03fb8d7565d5
memorysohigh <455501628@qq.com>
2020-11-25 17:30:07 +0800

    11.23

--- demo.book.11.23.js
+++ demo.book.11.23.js
@@ -1,16 +1,16 @@
-// let context = 'red,blue,black,yellow'
-// let value=context.split(/[^,]+/);
-// console.log(value);
-// let arry=[1,2,3,4,56,7,8]
-// let n=arry.slice(1,2)
-// let m=arry.splice(1,2)
+let context = 'red,blue,black,yellow'
+let value = context.split(/[^,]+/);
+console.log(value);
+let arry = [1, 2, 3, 4, 56, 7, 8]
+let n = arry.slice(1, 2)
+let m = arry.splice(1, 2)
 
-// console.log(n);
-// console.log(m);
-// let localeCompareValue = context.localeCompare('z')
-// console.log(localeCompareValue);
-// let a= Math.floor(2*Math.random()+8)
-// console.log(a);
+console.log(n);
+console.log(m);
+let localeCompareValue = context.localeCompare('a') //首字母比大小
+console.log('首字母比大小:' + localeCompareValue);
+let a = Math.floor(2 * Math.random() + 8)
+console.log(a);
 
 function person() {
 
@@ -26,3 +26,38 @@ man.prototype = new person()
 
 // var man1 = new man('liufu',28)
 console.log(man.Name);
+
+
+function person() {}
+let child = new person;
+console.log(person[Symbol.hasInstance](child));
+
+let arr = [1, 2, 3, 4, 5, 7]
+for (const iterator of arr[Symbol.iterator]()) {
+    console.log(iterator);
+}
+
+
+let dd = Object.getOwnPropertyNames(Function.prototype)
+let kk = Object.getOwnPropertySymbols(Function.prototype)
+let mm = Function.prototype.hasOwnProperty('Symbol(Symbol.hasInstance)')
+console.log(dd);
+console.log(kk);
+console.log(mm);
+
+let obj2 = {
+    name: 'shasha',
+    age: 25,
+    sex: 'women',
+    math: [1, 2, 3, 4, 5, 6, 7],
+    length: 4
+}
+console.log(Array.from(obj2.math));
+
+Array.from(obj2.math, function (x) { //映射函数等同于 .map()
+    console.log(this.name + "\'s" + ':' + x);
+}, {
+    name: 'shasha'
+})
+let c = []
+console.log(Array.isArray(c));
