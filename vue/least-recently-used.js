71523286f82e270be73efa15982b9ebf6b0b1869
WangJianHang <455501628@qq.com>
2021-08-20 08:24:01 +0800

    vue3-helloworld

---
+++ vue/least-recently-used.js
@@ -0,0 +1,11 @@
+const lru = require('lru-cache')
+
+let cache = new lru({
+    max: 500,
+    maxAge: 2000
+})
+cache.set('a', 111111111111)
+console.log(cache.get('a'));
+setTimeout(() => {
+    console.log(cache.get('a'));
+}, 1500);
