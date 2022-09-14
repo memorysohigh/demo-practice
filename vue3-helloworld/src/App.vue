b40fa743ddf88d1ad7f191a76efd1b1f86ff43b5
memorysohigh <wangjianhang@msunsoft.com>
2022-09-14 08:32:29 +0800

    阿萨德

--- vue3-helloworld/src/App.vue
+++ vue3-helloworld/src/App.vue
@@ -1,16 +1,28 @@
 <template>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div ref="root"></div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue"
+import * as all from 'vue'
 
 export default {
     name: "App",
     components: {
         HelloWorld,
     },
+  setup(){
+      const root = all.ref(null)
+    all.onMounted(()=>{
+      console.log(root)
+    })
+    return root
+  },
+  mounted(){
+    console.log(all)
+  }
 }
 </script>
 
